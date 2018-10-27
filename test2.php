b01126740e9f8691378f8289da0e0fd0c5671d02
masimansari <sd@aitchison.edu.pk>
2018-10-27 11:07:12 +0500

    First time host the site on Github.

---
+++ test2.php
@@ -0,0 +1,40 @@
+<?php
+echo "This is PHP output.";
+?>
+<!doctype html>
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<title>Test Page 2</title>
+</head>
+
+<body>
+	<div>
+		<h2>Heading 2</h2>
+	</div>
+	<div>
+		<h3>Heading 3</h3>
+	</div>
+	<div>
+		<h4>Heading 4</h4>
+	</div>
+	<div>
+		<h5>Heading 5</h5>
+	</div>
+
+	<div>
+		<p>This is new testing message.</p>
+	</div>
+	<div>
+		<p>This is second testing message.</p>
+	</div>
+	<div>
+		<p>This is third testing message.</p>
+	</div>
+	<div>
+		<p>This is fourth testing message.</p>
+	</div>
+</body>
+
+</html>
